f7835f2c19f1d40d92f290a8c9678619e45c2d77
Lluis Hernandez Saladie <lluis.hernandez.saladie@gmail.com>
2013-08-03 09:59:58 +0100

    Fixed the problem with the SQL initial data.

---
+++ simpleexample/profile_interface_app/sql/sectionssetup.sql
@@ -0,0 +1,5 @@
+INSERT INTO profile_interface_app_sectionssetup (section_name, section_order, section_enabled, section_component_template) VALUES ('Contact Information', 0, 1, 'cv/components/contact_information_section.html');
+INSERT INTO profile_interface_app_sectionssetup (section_name, section_order, section_enabled, section_component_template) VALUES ('Summary', 1, 1, 'cv/components/summary_section.html');
+INSERT INTO profile_interface_app_sectionssetup (section_name, section_order, section_enabled, section_component_template) VALUES ('Position', 2, 1, 'cv/components/positions_section.html');
+INSERT INTO profile_interface_app_sectionssetup (section_name, section_order, section_enabled, section_component_template) VALUES ('Education', 3, 1, 'cv/components/education_section.html');
+INSERT INTO profile_interface_app_sectionssetup (section_name, section_order, section_enabled, section_component_template) VALUES ('Skills', 4, 1, 'cv/components/skills_section.html');
---
+++ simpleexample/profile_interface_app/sql/socialsetup.sql
@@ -0,0 +1,6 @@
+INSERT INTO profile_interface_app_socialsetup (social_name, social_url, social_enabled, social_icon) VALUES ('LinkedIn', '', 1, 'icon-linkedin');
+INSERT INTO profile_interface_app_socialsetup (social_name, social_url, social_enabled, social_icon) VALUES ('Facebook', '', 1, 'icon-facebook');
+INSERT INTO profile_interface_app_socialsetup (social_name, social_url, social_enabled, social_icon) VALUES ('GooglePlus', '', 1, 'icon-google-plus');
+INSERT INTO profile_interface_app_socialsetup (social_name, social_url, social_enabled, social_icon) VALUES ('Pinterest', '', 1, 'icon-pinterest');
+INSERT INTO profile_interface_app_socialsetup (social_name, social_url, social_enabled, social_icon) VALUES ('Twitter', '', 1, 'icon-twitter');
+INSERT INTO profile_interface_app_socialsetup (social_name, social_url, social_enabled, social_icon) VALUES ('Github', '', 1, 'icon-github');
